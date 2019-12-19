b4167d47f4c25a4da60b1cbaa553ac8c486c3166
xianding <xianding@soloyao>
2019-12-19 11:25:02 +0800

    修改启动类类名

--- src/main/java/com/hnkc/KshAdminApplication.java
+++ src/main/java/com/hnkc/KshAdminApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QxOracleApplication {
+public class KshAdminApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(QxOracleApplication.class, args);
+		SpringApplication.run(KshAdminApplication.class, args);
 	}
 }
--- src/test/java/com/hnkc/test/TestJunit.java
+++ src/test/java/com/hnkc/test/TestJunit.java
@@ -6,14 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.hnkc.QxOracleApplication;
+import com.hnkc.KshAdminApplication;
 import com.hnkc.mapper.PermissionMapper;
 import com.hnkc.mapper.UserMapper;
 import com.hnkc.service.PermissionService;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = QxOracleApplication.class)
+@SpringBootTest(classes = KshAdminApplication.class)
 public class TestJunit {
 	@Autowired UserMapper userMapper;
 	@Autowired PermissionMapper permissionMapper;
