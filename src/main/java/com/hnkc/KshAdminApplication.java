7990481ffa02cf863e345c4eebbb8562913a71be
xianding <xianding@soloyao>
2019-12-20 13:32:58 +0800

    控制台输出mybatis执行sql语句，将用户管理页面的代理机构布局加上

--- src/main/java/com/hnkc/KshAdminApplication.java
+++ src/main/java/com/hnkc/KshAdminApplication.java
@@ -2,10 +2,23 @@ package com.hnkc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KshAdminApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KshAdminApplication.class, args);
+	}
+	
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				container.setSessionTimeout(1800);
+			}
+		};
 	}
 }
